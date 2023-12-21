039552b7bbc6555043fdcbfd1613eb2910630ad0
CaloMorell <oscarmorell07@yahoo.com>
2023-12-21 18:26:23 -0400

    0. Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """The script ensures proper error handling for potential issues during API
 requests and follows the specified guidelines for formatting and output. """
 
-"""import requests
+import requests
 import sys
 
 def make_api_request(url):
@@ -52,36 +52,4 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # Call the function with the provided employee ID
-    get_employee_todo_progress(employee_id)"""
-
-import requests
-import sys
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} employee_id(int)")
-        sys.exit(1)
-
-    API_URL = "https://jsonplaceholder.typicode.com"
-    EMPLOYEE_ID = sys.argv[1]
-
-    response = requests.get(
-        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
-        params={"_expand": "user"}
-    )
-    data = response.json()
-
-    if not len(data):
-        print("RequestError:", 404)
-        sys.exit(1)
-
-    employee_name = data[0]["user"]["name"]
-    total_tasks = len(data)
-    done_tasks = [task for task in data if task["completed"]]
-    total_done_tasks = len(done_tasks)
-
-    print(f"Employee {employee_name} is done with tasks"
-        f"({total_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task['title']}")
+    get_employee_todo_progress(employee_id)
