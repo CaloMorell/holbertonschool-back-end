1c9f4bd574c3efcf95f643196b326f314185804d
CaloMorell <oscarmorell07@yahoo.com>
2023-12-21 18:51:38 -0400

    0. Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,6 +5,34 @@ requests and follows the specified guidelines for formatting and output. """
 import requests
 import sys
 
+def get_employee_todo_progress(employee_id):
+    try:
+        # Make a GET request to the API to obtain employee TODO list
+        todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+        user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+
+        # Check if the requests were successful
+        todos_response.raise_for_status()
+        user_response.raise_for_status()
+
+        todos = todos_response.json()
+        user_data = user_response.json()
+
+        # Extract relevant information
+        employee_name = user_data.get('name', f'Employee {employee_id}')
+        done_tasks = [todo['title'] for todo in todos if todo['completed']]
+        total_tasks = len(todos)
+
+        # Display the information in the specified format
+        print(f'Employee {employee_name} is done with tasks ({len(done_tasks)}/{total_tasks}):')
+        print(f'\t{employee_name}: {len(done_tasks)}/{total_tasks}')
+
+        for task_title in done_tasks:
+            print(f'\t {task_title}')
+
+    except requests.exceptions.RequestException as e:
+        # Handle request errors
+        print(f'Error during request: {e}')
 
 if __name__ == '__main__':
     # Check if the script is called with the correct number of arguments
@@ -12,49 +40,8 @@ if __name__ == '__main__':
         print('Usage: python script.py <EMPLOYEE_ID>')
         sys.exit(1)
 
-
-def make_api_request(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f'Error during request: {e}')
-        return None
-
-def get_employee_todo_progress(employee_id):
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-
-    # Make API requests
-    todos = make_api_request(todos_url)
-    user_data = make_api_request(user_url)
-
-    if todos is None or user_data is None:
-        # Exit or handle errors accordingly
-        sys.exit(1)
-
-    # Extract relevant information
-    employee_name = user_data.get('name', f'Employee {employee_id}')
-    done_tasks = [todo['title'] for todo in todos if todo['completed']]
-    total_tasks = len(todos)
-
-    # Display the information in the specified format
-    print(f'Employee {employee_name} is done with tasks
-        ({len(done_tasks)}/{total_tasks}):')
-    print(f'\t{employee_name}: {len(done_tasks)}/{total_tasks}')
-
-    for task_title in done_tasks:
-        print(f'\t {task_title}')
-
-
-
     # Get the employee ID from the command line argument
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print('Invalid employee ID. Please provide a valid integer.')
-        sys.exit(1)
+    employee_id = sys.argv[1]
 
     # Call the function with the provided employee ID
     get_employee_todo_progress(employee_id)
