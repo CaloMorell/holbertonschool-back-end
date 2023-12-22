db5d6e296c41d85c0789ca185025674aac778a1c
CaloMorell <oscarmorell07@yahoo.com>
2023-12-22 16:56:01 -0400

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,7 +6,6 @@ import json
 import requests
 import sys
 
-
 def get_employee_todo_progress(employee_id):
     """
     Returns information about his/her TODO list progress.
@@ -17,27 +16,23 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()  # Parse user names to JSON
     employee_name = user_data['name']
 
-
-#  Get the user's tasks through the API
+    # Get the user's tasks through the API
     todo_response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            employee_id))
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
     todo_data = todo_response.json()  # Parse user's tasks to JSON
     total_tasks = len(todo_data)  # Get the total number of tasks
 
-
-# Get the number of completed tasks
+    # Get the number of completed tasks
     done_tasks = [task for task in todo_data if task['completed'] is True]
     num_done_tasks = len(done_tasks)  # Get  the number of completed tasks
 
-# Print the user's tasks
+    # Print the user's tasks
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, num_done_tasks, total_tasks))
     for task in done_tasks:
         print("\t {}".format(task['title']))
 
-
-# Save the information in the JSON file
+    # Save the information in the JSON file
     with open('{}.json'.format(employee_id), 'w') as jsonfile:
         json.dump({employee_id: [{
             "task": task['title'],
@@ -45,7 +40,12 @@ def get_employee_todo_progress(employee_id):
             "username": employee_name
         } for task in todo_data]}, jsonfile)
 
-
 if __name__ == '__main__':
+    # Check if the required command-line argument is provided
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Convert the command-line argument to an integer (employee_id)
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
