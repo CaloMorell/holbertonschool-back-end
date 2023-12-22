d2fea437f13dfbf389a7a4b720bca3fb6c84b515
CaloMorell <oscarmorell07@yahoo.com>
2023-12-22 15:51:19 -0400

    3. Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -2,7 +2,8 @@
 
 """Python script to export data in the JSON format
 """
-"""import json
+
+import json
 import requests
 from collections import defaultdict
 
@@ -11,7 +12,7 @@ todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
 def export_all_to_json():
-    Export all tasks from all employees to JSON
+    """Export all tasks from all employees to JSON"""
 
     correct_output = defaultdict(list)
 
@@ -45,34 +46,4 @@ def export_all_to_json():
 
 
 if __name__ == "__main__":
-    export_all_to_json()"""
-    
-import json
-import requests
-from sys import argv
-
-
-if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(base_url + "users").json()
-
-    all_data = {}
-    for user in users:
-        user_id = str(user.get("id"))
-        username = user.get("username")
-
-        tasks = requests.get(base_url + "todos",
-                        params={"userId": user_id}).json()
-
-        task_list = []
-        for task in tasks:
-            task_list.append({
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-            })
-
-        all_data[user_id] = task_list
-
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_data, json_file)
+    export_all_to_json()
